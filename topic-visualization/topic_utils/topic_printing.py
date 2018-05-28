3af3c1ef1e5581b8d42ccefbb87d550bcd418228
Victor Künstler <victor.kuenstler@outlook.com>
2018-05-28 21:07:06 +0200

    Ad author topic distribution printing and index fixes

--- topic-visualization/topic_utils/topic_printing.py
+++ topic-visualization/topic_utils/topic_printing.py
@@ -1,8 +1,14 @@
 import pandas as pd
 
 
-def print_topic_df(model, topic_id, topn=20):
+def print_author_topics(model, author_id):
+    df = pd.DataFrame(model[author_id], columns=['Topic', 'Value'])
+    df = df.sort_values(by='Value', ascending=False)
+    return df.set_index('Topic')
+
+
+def print_topic(model, topic_id, topn=20):
     d = model.print_topic(topic_id, topn).replace('"', '').split('+')
     d = [el.split('*') for el in d]
     df = pd.DataFrame(d, columns=['prob', 'word'])
-    return df
+    return df.set_index('word')
